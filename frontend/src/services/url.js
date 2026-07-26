f997e6a4cb7f4f24fcc51e09b03b3dba38616673
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2026-07-26 12:51:26 +0530

    some change

--- frontend/src/services/url.js
+++ frontend/src/services/url.js
@@ -1,3 +1,3 @@
-const url = "http://localhost:6200";
-// const url = "https://twondinnings-mediversal.onrender.com";
+// const url = "http://localhost:6200";
+const url = "https://twondinnings-mediversal-3ijz.onrender.com";
 export { url };
